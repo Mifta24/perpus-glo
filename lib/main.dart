a36c7db83d18997c2131fbe14e6016fd8211eb2e
Mifta Aldi <miftaadit19@gmail.com>
2025-05-04 20:05:18 +0700

    integrate register page with main app entry

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../firebase_options.dart';
-import 'core/router.dart';
 import 'core/services/firebase_service.dart';
 import 'core/theme/app_theme.dart';
+import 'core/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,29 +22,4 @@ class MyApp extends ConsumerWidget {
       debugShowCheckedModeBanner: false,
     );
   }
-}
-
-class FirebaseService {
-  static Future<void> initialize() async {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    
-    // Set up Firebase Messaging
-    final messaging = FirebaseMessaging.instance;
-    await messaging.requestPermission();
-    
-    // Get FCM token
-    String? token = await messaging.getToken();
-    print('FCM Token: $token');
-  }
-  
-  // Firebase Auth instance
-  static FirebaseAuth get auth => FirebaseAuth.instance;
-  
-  // Firestore instance
-  static FirebaseFirestore get firestore => FirebaseFirestore.instance;
-  
-  // Messaging instance
-  static FirebaseMessaging get messaging => FirebaseMessaging.instance;
 }
